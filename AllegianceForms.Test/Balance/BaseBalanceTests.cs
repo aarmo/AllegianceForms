bc59bc973d3e91a0a0aa1bc57c826cb1d5a8a7af
Aaron <aaron.j.moore@gmail.com>
2017-02-22 22:04:01 +1000

    cap ships

---
+++ AllegianceForms.Test/Balance/BaseBalanceTests.cs
@@ -0,0 +1,44 @@
+﻿using AllegianceForms.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllegianceForms.Test.Balance
+{
+    [TestClass]
+    public class BaseBalanceTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            StrategyGame.LoadData();
+            StrategyGame.ResetGame(GameSettings.Default());
+        }
+
+        [TestMethod]
+        public void BaseCostsAreBalanced()
+        {
+            var bases = StrategyGame.Bases.Bases;
+            var tech = StrategyGame.TechTree[0].TechItems;
+            var results = new List<float>();
+
+            foreach (var b in bases)
+            {
+                var name = b.Type.ToString() + " Constructor";
+                var con = tech.FirstOrDefault(_ => _.Type == ETechType.Construction && _.Name == name);
+                if (con == null) continue; 
+
+                var factor = (1f * b.Health + b.ScanRange + b.ScanRange) / (con.Cost + con.DurationSec);
+                results.Add(factor);
+            }
+
+            var diff = results.Max() - results.Min();
+
+            diff.ShouldBeLessThan(0.1f);
+        }
+    }
+}
--- Doc/Roadmap.md
+++ Doc/Roadmap.md
@@ -7,7 +7,7 @@ Next Release Roadmap
 - Display critical and info messages (press a key to go to a recent critical alert)
 - ~~Add global upgrades to the tech tree (ship speed, health, signature, ship/base laser/missile damage, tracking, fire rate, resource conversion, etc.)~~
 - ~~Add more towers (missile/repair)~~
-- Add capital ships (shipyard tech. Light: Corvette. Medium: Frigate, Destroyer. Heavy: Cruiser, Devastator. Super Heavy: Battlecruiser, Battleship)
+- ~~Add capital ships (shipyard tech. Light: Corvette. Medium: Frigate, Destroyer. Heavy: Cruiser, Devastator. Super Heavy: Battlecruiser, Battleship)~~
 - AI improvements (Use scouts to repair bombers, support troop transports)
 - Faction differences (Random faction stats, integrate faction editor, save/load presets)
 
